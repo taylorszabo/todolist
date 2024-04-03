c91e325e1eb52749fd8648299620f79ecb44b6b3
someoldguy88 <logan008@msn.com>
2024-04-03 15:53:03 -0400

    Enable todolist editing.

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -10,6 +10,10 @@ class TodoList extends Component
     public $todos;
     public $newTodo;
 
+    public $updateTodo;
+
+    public bool $isEditing = false;
+
     public function mount()
     {
         $this->todos = Todo::all();
@@ -20,7 +24,7 @@ class TodoList extends Component
         return view('livewire.todo-list');
     }
 
-    public function addTodo()
+    public function addTodo() :void
     {
         if($this->newTodo !== '' && $this->newTodo !== null)
         {
@@ -31,17 +35,35 @@ class TodoList extends Component
 
     }
 
-    public function toggleCompleted($todoId)
+    public function toggleCompleted($todoId) :void
     {
         $todo = Todo::find($todoId);
         $todo->update(['completed' => !$todo->completed]);
         $this->todos = Todo::all();
     }
 
-    public function deleteTodo($todoId)
+    public function deleteTodo($todoId) :void
     {
         Todo::destroy($todoId);
         $this->todos = Todo::all();
+    }
+
+    public function editTodo($todoId, $todoName) :void
+    {
+        $todo = Todo::find($todoId);
+        $todo->update(['name' => $todoName]);
+        $this->todos = Todo::all();
+        $this->isEditing = false;
+    }
+
+    public function enableEditing() :void
+    {
+        $this->isEditing = true;
+    }
+
+    public function cancelEditing() :void
+    {
+        $this->isEditing = false;
     }
 }
 
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,20 +1,37 @@
-<div class="bg-amber-300 p-8 flex float-left">
+<div class="bg-amber-300 p-8 flex float-left relative">
+    <div class="absolute top-2.5 right-8">
+        @if (!$isEditing)
+            <button wire:click="enableEditing" class="p-0.5 font-bold text-black">Edit</button>
+        @else
+            <button wire:click="cancelEditing" class="p-0.5 font-bold text-red-600">Cancel</button>
+        @endif
+    </div>
     <form @submit.prevent>
-        <input wire:model="newTodo" type="text" class="bg-amber-300 " placeholder="Add a new todo...">
-        <button wire:click="addTodo" hidden>Add</button>
-        <hr>
-        <ul>
-            @foreach ($todos as $todo)
-                <li class="pt-1.5 pb-1.5">
-                    <input type="checkbox" class="mr-1 " wire:click="toggleCompleted({{ $todo->id }})" {{ $todo->completed ? 'checked' : '' }}>
-                    <span class="{{ $todo->completed ? 'line-through' : '' }}">
-                       {{ $todo->name }}
-                    </span>
-                    <button wire:click="deleteTodo({{ $todo->id }})" class="float-right p-0.5 font-bold">X</button>
-                </li>
-                <hr>
-            @endforeach
-        </ul>
+        <div>
+            <input wire:model="newTodo" type="text" class="bg-amber-300 w-full" placeholder="Add a new todo..." />
+            <button wire:click="addTodo" disabled>Add</button>
+            <hr>
+            <ul>
+                @foreach ($todos as $todo)
+                    <div class="pt-1.5 pb-1.5">
+                        <li>
+                            @if (!$isEditing)
+                                <input type="checkbox" class="mr-1 " wire:click="toggleCompleted({{ $todo->id }})" {{ $todo->completed ? 'checked' : '' }} />
+                                <span class="{{ $todo->completed ? 'line-through' : '' }}">
+                               {{ $todo->name }}
+                            </span>
+                            @else
+                                <input id="updateTodo{{$todo->id}}" name="updateTodo{{$todo->id}}" value="{{ $todo->name }}" class="bg-amber-100 h-full"/>
+                                <button wire:click="editTodo({{ $todo->id }}, document.getElementById('updateTodo{{$todo->id}}').value)" class="float-right p-0.5 font-bold text-black">edit</button>
+                                <button wire:click="deleteTodo({{ $todo->id }})" class="float-right p-0.5 font-bold text-red-600">X</button>
+                            @endif
+                        </li>
+                        <hr>
+                    </div>
+                @endforeach
+            </ul>
+        </div>
     </form>
 </div>
+
 
